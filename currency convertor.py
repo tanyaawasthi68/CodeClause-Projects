4d2a8439e983c54dcc3f6cb95091036fffd82ce7
tanyaawasthi68 <112073793+tanyaawasthi68@users.noreply.github.com>
2024-04-30 19:18:18 +0530

    Update currency convertor.py

--- currency convertor.py
+++ currency convertor.py
@@ -5,29 +5,16 @@ import tkinter.messagebox
 
 root=tk.Tk()
 root.title("Currency Conversion")
-
-
-
 root.configure(bg = '#e6e5e5')
 root.geometry("700x400")
 
 headlabel = tk.Label(root,font=('lato black', 19,'bold'), text = '         Python Project   :    Currency Converter  ', bg= '#663300',fg='white') 
 headlabel.grid(row=1, column=0,sticky=W)
-
-
 variable1 = tk.StringVar(root) 
 variable2 = tk.StringVar(root) 
- 
 variable1.set("currency") 
 variable2.set("currency") 
-
-
-    
 CurrencyCode_list= ["INR","USD","CAD","CNY","DKK","EUR"]
-
-
-
-
 def RealTimeCurrencyConversion():
     from forex_python.converter import CurrencyRates
     c= CurrencyRates()
@@ -51,18 +38,14 @@ def clear_all():
 Label_1 =Label(root, font=('lato black', 27,'bold'), text="",padx=2,pady=2, bg="#e6e5e5",fg ="black")
 Label_1.grid(row=1, column=0,sticky=W)
 
-
 label1= tk.Label(root,font=('lato black',15,'bold'),text="\t  Amount :  ",bg="#e6e5e5", fg="black")
 label1.grid(row=2,column=0,sticky=W)                 
-
 
 label1= tk.Label(root,font=('lato black',15,'bold'),text="\t  From Currency  :  ",bg="#e6e5e5", fg="black")
 label1.grid(row=3,column=0,sticky=W)     
 
-
 label1= tk.Label(root,font=('lato black',15,'bold'),text="\t  To Currency  :  ",bg="#e6e5e5", fg="black")
 label1.grid(row=4,column=0,sticky=W)     
-
 
 label1= tk.Label(root,font=('lato black',15,'bold'),text="         Converted Amount :",bg="#e6e5e5", fg="black")
 label1.grid(row=8,column=0,padx=2,sticky=W)   
@@ -72,9 +55,6 @@ Label_1.grid(row=5, column=0,sticky=W)
 
 Label_1 =Label(root, font=('lato black', 7,'bold'), text="",padx=2,pady=2, bg="#e6e5e5",fg ="black")
 Label_1.grid(row=7, column=0,sticky=W)
-
-
-
 
 FromCurrency_option= tk.OptionMenu(root,variable1,*CurrencyCode_list) 
 ToCurrency_option= tk.OptionMenu(root,variable2,*CurrencyCode_list)
@@ -88,17 +68,14 @@ Amount1_field.grid(row=2,column=0,sticky=E)
 Amount2_field= tk.Entry(root)
 Amount2_field.grid(row=8,column=0,sticky=E)   
 
-
 Label_9= Button(root,font=("arial",15,"bold"),text="  Convert  ",padx=2,pady=2,bg="#e6e5e5", fg="black",command= RealTimeCurrencyConversion)
 Label_9.grid(row=6,column=0)
 
 Label_1 =Label(root, font=('lato black', 7,'bold'), text="",padx=2,pady=2, bg="#e6e5e5",fg ="black")
 Label_1.grid(row=9, column=0,sticky=W)
 
-
 Label_9= Button(root,font=("arial",15,"bold"),text="  Clear All ",padx=2,pady=2,bg="white", fg="red",command= clear_all)
 Label_9.grid(row=10,column=0)
-
 
 root.mainloop()
 
